67bdb4f12c81229f538b73ff62ae4d034e085633
evlx87 <evlx1311@gmail.com>
2026-04-09 16:04:25 +0300

    Обновления employees/admin.py и training/admin.py

--- employees/admin.py
+++ employees/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Department, Position, Employee
 
-# Register your models here.
-admin.site.register(Department)
-admin.site.register(Position)
-admin.site.register(Employee)
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name", "department")
+    search_fields = ("name", "department__name")
+    list_filter = ("department",)
+    autocomplete_fields = ("department",)
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "position", "hire_date", "is_active")
+    search_fields = ("last_name", "first_name", "patronymic", "position__name")
+    list_filter = ("is_active", "position__department")
+    autocomplete_fields = ("position",)
--- training/admin.py
+++ training/admin.py
@@ -3,13 +3,14 @@ from .models import TrainingRecord, PositionTrainingRequirement
 
 @admin.register(TrainingRecord)
 class TrainingRecordAdmin(admin.ModelAdmin):
-    list_display = ("employee", "category", "provider", "training_date", "expiry_date", "is_expired", "is_archived")
-    list_filter = ("category", "provider", "is_archived")
-    search_fields = ("employee__last_name", "certificate")
+    list_display = ("employee", "category", "provider", "training_date", "expiry_date", "is_expired")
+    list_filter = ("category", "provider")
+    search_fields = ("employee__last_name", "employee__first_name", "certificate")
     autocomplete_fields = ("employee",)
 
 @admin.register(PositionTrainingRequirement)
 class PositionTrainingRequirementAdmin(admin.ModelAdmin):
     list_display = ("position", "category")
     list_filter = ("category",)
+    search_fields = ("position__name",)
     autocomplete_fields = ("position",)
